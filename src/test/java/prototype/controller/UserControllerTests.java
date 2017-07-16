3fd8e3d2ddfa3464049a29aa6e2da120cbf92480
dat_tieu <tieutruongdat@gmail.com>
2017-07-17 01:34:16 +0700

    Begin implementing unit test for Controllers

---
+++ src/test/java/prototype/controller/UserControllerTests.java
@@ -0,0 +1,54 @@
+package prototype.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import prototype.config.AppConfig;
+import prototype.model.User;
+import prototype.service.UserService;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+@WebAppConfiguration
+public class UserControllerTests {
+
+	@Mock
+	private UserService userService;
+	
+	@InjectMocks
+	private UserController userController;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
+	
+	@Test
+	public void testGetUser() throws Exception {
+		User user = new User("admin@gmail.com", "admin123", null, null);
+		
+		when(userService.getByKey("email", "admin@gmail.com")).thenReturn(user);
+		
+		mockMvc.perform(get("/users").param("email", "admin@gmail.com"))
+        		.andExpect(status().isOk());
+		
+	}
+	
+	
+}
