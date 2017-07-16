1d19e4af7604a24652044dd5865c0b7c9350b9ba
dat_tieu <tieutruongdat@gmail.com>
2017-07-16 18:39:26 +0700

    Fix something wrong!

---
+++ src/main/java/prototype/constant/Properties.java
@@ -0,0 +1,22 @@
+package prototype.constant;
+
+// FIXME this file should not be in this package
+public final class Properties {
+
+	public static enum UserRole {
+		ADMIN, USER, GUEST
+	}
+	
+	public static enum VehicleState {
+		INPARK, OUTPARK
+	}
+	
+	public static enum VehicleModel {
+		BIKE, MOTORBIKE, CAR, TRUCK
+	}
+	
+	public static enum PaymentType {
+		CREDIT, DEBIT, ACCOUNT
+	}
+	
+}
